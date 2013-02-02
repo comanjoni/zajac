22d15343a4fdf029f012dc36a8de1b35b7fa28dc
Aleksandar Bogdanovic <comanjoni@gmail.com>
2013-02-02 19:30:09 +0100

    List, LIstItemRenderer, AbstractListItemRenderer clean state

--- be/zajac/renderers/AbstractListItemRenderer.hx
+++ be/zajac/renderers/AbstractListItemRenderer.hx
@@ -19,6 +19,16 @@ class AbstractListItemRenderer extends StyledComponent, implements ISkin {
 	inline static public var OVER:		String = 'over';
 	inline static public var SELECTED:	String = 'selected';
 	
+	//******************************
+	//		PUBLIC VARIABLES
+	//******************************
+	
+	public var data(default, set_data): Dynamic;
+	public var selected(default, set_selected): Bool = false;
+	
+	//******************************
+	//		PUBLIC METHODS
+	//******************************
 	
 	public function new() {
 		super();
@@ -26,20 +36,51 @@ class AbstractListItemRenderer extends StyledComponent, implements ISkin {
 		defaultHeight = FWCore.getHeightUnit();
 	}
 	
+	public function draw(client: BaseComponent, states:Hash<DisplayObject>): Void { }
+	
+	//******************************
+	//		PRIVATE METHODS
+	//******************************
+	
 	override public function initialize(): Void {
 		addEventListener(MouseEvent.CLICK, _onClick);
 		#if !(android || ios)
 		addEventListener(MouseEvent.ROLL_OVER, _onRollOver);
 		addEventListener(MouseEvent.ROLL_OUT, _onRollOut);
 		#end
+		
 		skin = this;
 		state = OUT;
 	}
+	
+	//******************************
+	//		GETTERS AND SETTERS
+	//******************************
 	
 	override private function get_state(): String {
 		if (selected) return SELECTED;
 		return state;
 	}
+	
+	private function set_data(v: Dynamic): Dynamic {
+		if (v != data) {
+			data = v;
+			invalidSkin();
+		}
+		return v;
+	}
+	
+	private function set_selected(v: Bool): Bool {
+		if (v != selected) {
+			selected = v;
+			invalidState();
+		}
+		return v;
+	}
+	
+	//******************************
+	//		EVENT LISTENERS
+	//******************************
 	
 	private function _onClick(evt: MouseEvent): Void {
 		dispatchEvent(new Event(Event.SELECT));
@@ -52,25 +93,5 @@ class AbstractListItemRenderer extends StyledComponent, implements ISkin {
 	private function _onRollOut(evt: MouseEvent): Void {
 		state = OUT;
 	}
-	
-	public var data(default, set_data): Dynamic;
-	private function set_data(v: Dynamic): Dynamic {
-		if (v != data) {
-			data = v;
-			invalidSkin();
-		}
-		return v;
-	}
-	
-	public var selected(default, set_selected): Bool = false;
-	private function set_selected(v: Bool): Bool {
-		if (v != selected) {
-			selected = v;
-			invalidState();
-		}
-		return v;
-	}
-	
-	public function draw(client: BaseComponent, states:Hash<DisplayObject>): Void { }
 	
 }
